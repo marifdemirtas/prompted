1fdfefb841e6592709e3b8c3f01a8b762aaf2a6e
marifdemirtas <12948961+marifdemirtas@users.noreply.github.com>
2025-04-21 17:21:37 -0500

    Serve build/ folder from client folder for deployment

--- server/index.js
+++ server/index.js
@@ -66,14 +66,14 @@ app.use('/api/users', requireAuth, userRoutes);
 app.use('/api/conversations', requireAuth, conversationRoutes);
 app.use('/api/llm', requireAuth, llmRoutes);
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-  
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-}
+// Serve static assets from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // Error handling middleware
 app.use((err, req, res, next) => {
